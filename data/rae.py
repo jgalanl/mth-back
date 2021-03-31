c19468cf7342d49d945bbd5b3ecdedec5bf5e85f
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-03-31 10:35:37 +0200

    add requirements.txt

--- data/rae.py
+++ data/rae.py
@@ -33,7 +33,7 @@ def transform(data):
         articles = data.findAll('article')
         word = {}
         word['definitions'] = []
-        word['complex'] = []
+        word['compound'] = []
 
         # Get lemma
         word['lemma'] = articles[0].find('header', attrs={'class': 'f'}).text
@@ -70,11 +70,11 @@ def transform(data):
                     # Para cada entrada formateamos su contenido en formato json
                     for definition_content in definitions_content:
                         definition = {}
-                        definition['aceptacion'] = int(definition_content.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
+                        definition['order'] = int(definition_content.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
                         if definition_content.find('abbr', attrs={'class': ['d', 'g']}):
-                            definition['categoria'] = definition_content.find('abbr', attrs={'class': ['d', 'g']})['title']
+                            definition['category'] = definition_content.find('abbr', attrs={'class': ['d', 'g']})['title']
                         if definition_content.find('abbr', attrs={'class': 'c'}):
-                            definition['campo'] = definition_content.find('abbr', attrs={'class': 'c'})['title']
+                            definition['field'] = definition_content.find('abbr', attrs={'class': 'c'})['title']
                         
                         definition_content = re.findall('.*?[.]', definition_content.text)
                         definition_text = ""
@@ -86,22 +86,22 @@ def transform(data):
                         # Extraer las formas complejas
                         for a in article.findAll('p', attrs={'class': ['k5', 'k6']}):
                             # collocations?
-                            complex_word = {}
-                            complex_word['forma'] = a.text
+                            compound = {}
+                            compound['forma'] = a.text
                             sibling = a.find_next_sibling('p')
-                            complex_word['aceptacion'] = int(sibling.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
-                            complex_word['categoria'] = sibling.find('abbr', attrs={'class': ['d', 'g']})['title']
+                            compound['order'] = int(sibling.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
+                            compound['category'] = sibling.find('abbr', attrs={'class': ['d', 'g']})['title']
                             
                             if sibling.find('abbr', attrs={'class': 'c'}):
-                                complex_word['pais'] = sibling.find('abbr', attrs={'class': 'c'})['title']
+                                compound['country'] = sibling.find('abbr', attrs={'class': 'c'})['title']
                                 definition_content = re.findall('.*?[.]', sibling.text)
                                 definition_text = ""
                                 for i in definition_content:
                                     if(len(i) > 8):
                                         definition_text += i
                                                             
-                                complex_word['definition'] = definition_text.strip()
-                            word['complex'].append(complex_word)
+                                compound['definition'] = definition_text.strip()
+                            word['compound'].append(compound)
 
         return json.dumps(word, ensure_ascii=False)  
     except:
@@ -116,106 +116,6 @@ def load():
     pass
 
 
-def get_definition_rae(word):
-    # if request.method == 'GET':
-    #     word = escape(request.args.get('word'))
-        # definition_list = list()
-        page = requests.get(url= 'https://dle.rae.es/' + word)
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.text, 'html.parser')
-            
-            
-            articles = soup.findAll('article')
-            palabra = {}
-            palabra['definiciones'] = []
-            palabra['complejas'] = []
-
-            try:
-
-                # Obtener el lema
-                palabra['lema'] = articles[0].find('header', attrs={'class': 'f'}).text
-                
-                # Especificar la fuente
-                palabra['fuente'] = 'rae'
-
-                # Comprobar si hay diferencia entre el masculino y el femenino
-                if ',' in palabra['lema']:
-                    # Si hay diferencia actualizamos el lema e incluimos el femenino
-                    palabra['lema'], palabra['femenino'] = palabra['lema'].split(', ')
-
-                # Comprobar si la entrada tiene una referencia a otra palabra (vease)
-                if articles[0].find('p', attrs={'class': 'l2'}):
-                    palabra['vease'] = articles[0].find('a', attrs={'class': 'a'}).text.replace('.','')
-                    
-                for article in articles:
-                    lemas = article.findAll('header', attrs={'class': 'f'})
-                    for lema in lemas:
-                        if lema.find('i') or article.find('p', attrs={'class': 'n2'}):
-                            palabra['etimologia'] = article.find('p', attrs={'class': 'n2'}).text
-                        
-                        if article.find('p', attrs={'class': 'n3'}):
-                            palabra['etimologia'] = article.find('p', attrs={'class': 'n3'}).text
-
-
-                        if lema.text.isupper():
-                            palabra['siglas']: True
-                        
-                        if '-' in lema.text:
-                            palabra['compositivo']: True
-
-                        if article.find('p', attrs={'class': 'n5'}):
-                            palabra['ortografia'] = article.find('p', attrs={'class': 'n5'}).text
-
-                        # Extraer las definiciones
-                        definitions_content = article.findAll('p', attrs={'class' : ['j', 'j1', 'j2']})
-                        if definitions_content:
-                            # Para cada entrada formateamos su contenido en formato json
-                            for definition_content in definitions_content:
-                                definition = {}
-                                definition['aceptacion'] = int(definition_content.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
-
-                                if definition_content.find('abbr', attrs={'class': ['d', 'g']}):
-                                    definition['categoria'] = definition_content.find('abbr', attrs={'class': ['d', 'g']})['title']
-                                if definition_content.find('abbr', attrs={'class': 'c'}):
-                                    definition['campo'] = definition_content.find('abbr', attrs={'class': 'c'})['title']
-
-                                definition_content = re.findall('.*?[.]', definition_content.text)
-                                definition_text = ""
-                                for i in definition_content:
-                                    if(len(i) > 8):
-                                        definition_text += i
-
-
-                                definition['definicion'] = definition_text.strip()
-                                palabra['definiciones'].append(definition)
-
-                            # Extraer las formas complejas
-                            for a in article.findAll('p', attrs={'class': ['k5', 'k6']}):
-                                forma_compleja = {}
-                                forma_compleja['forma'] = a.text
-                                sibling = a.find_next_sibling('p')
-
-                                forma_compleja['aceptacion'] = int(sibling.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
-                                forma_compleja['categoria'] = sibling.find('abbr', attrs={'class': ['d', 'g']})['title']
-
-                                if sibling.find('abbr', attrs={'class': 'c'}):
-                                    forma_compleja['pais'] = sibling.find('abbr', attrs={'class': 'c'})['title']
-
-                                definition_content = re.findall('.*?[.]', sibling.text)
-                                definition_text = ""
-                                for i in definition_content:
-                                    if(len(i) > 8):
-                                        definition_text += i
-                                                            
-                                forma_compleja['definicion'] = definition_text.strip()
-                                palabra['complejas'].append(forma_compleja)
-
-                return json.dumps(palabra, ensure_ascii=False)  
-            except:
-                print("Se ha producido un error para la entrada {}".format(word))
-
-        else:
-            print("Error de conexión")
 
 def main():
     # print(get_definition_rae('inteligencia'))
