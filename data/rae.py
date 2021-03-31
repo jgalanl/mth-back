2bdc5a8f948c6560954e5f546e65947de4fa9f56
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-03-31 13:42:17 +0200

    Modify transformation method

--- data/rae.py
+++ data/rae.py
@@ -2,8 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
+import pymongo
 
 RAE_URL = 'https://dle.rae.es/'
+
+f = open("credentials.txt", "r")
+cred = f.read()
 
 def extraction(word):
     try:    
@@ -11,7 +15,6 @@ def extraction(word):
         if page.status_code == 200:
             soup = BeautifulSoup(page.text, 'html.parser')
             articles = soup.findAll('article')
-
             if len(articles) > 0:
                 return {
                     'result': True, 
@@ -32,20 +35,15 @@ def transform(data):
     try:
         articles = data.findAll('article')
         word = {}
-        word['definitions'] = []
-        word['compound'] = []
-
+        word['articles'] = []
         # Get lemma
         word['lemma'] = articles[0].find('header', attrs={'class': 'f'}).text
-                
         # Set source
         word['source'] = 'rae'
-
         # Check masculine-feminine orthopraphy
         if ',' in word['lemma']:
             # If they are not equal update lemma and adding feminine
             word['lemma'], word['feminine'] = word['lemma'].split(', ')
-
         # Check references of the word (vease)
         if articles[0].find('p', attrs={'class': 'l2'}):
             word['references'] = articles[0].find('a', attrs={'class': 'a'}).text.replace('.','')
@@ -53,21 +51,23 @@ def transform(data):
         for article in articles:
             lemmas = article.findAll('header', attrs={'class': 'f'})
             for lemma in lemmas:
+                lemma_dict = {}
                 if lemma.find('i') or article.find('p', attrs={'class': 'n2'}):
-                    word['etymology'] = article.find('p', attrs={'class': 'n2'}).text
+                    lemma_dict['etymology'] = article.find('p', attrs={'class': 'n2'}).text
                 if article.find('p', attrs={'class': 'n3'}):
-                    word['etymology'] = article.find('p', attrs={'class': 'n3'}).text
+                    lemma_dict['etymology'] = article.find('p', attrs={'class': 'n3'}).text
                 if lemma.text.isupper():
-                    word['acronym']: True
+                    lemma_dict['acronym']: True
                 if '-' in lemma.text:
-                    word['compound']: True
+                    lemma_dict['compound']: True
                 if article.find('p', attrs={'class': 'n5'}):
-                    word['orthography'] = article.find('p', attrs={'class': 'n5'}).text
-                    
-                # Extraer las definiciones
+                    lemma_dict['orthography'] = article.find('p', attrs={'class': 'n5'}).text
+                                    
+                # Get list of definitions
                 definitions_content = article.findAll('p', attrs={'class' : ['j', 'j1', 'j2']})
                 if definitions_content:
-                    # Para cada entrada formateamos su contenido en formato json
+                    # Formating entry to json model
+                    lemma_dict['definitions'] = []
                     for definition_content in definitions_content:
                         definition = {}
                         definition['order'] = int(definition_content.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
@@ -81,50 +81,57 @@ def transform(data):
                         for i in definition_content:
                             if(len(i) > 8):
                                 definition_text += i
-                                definition['definition'] = definition_text.strip()
-                                word['definitions'].append(definition)
-                        # Extraer las formas complejas
-                        for a in article.findAll('p', attrs={'class': ['k5', 'k6']}):
-                            # collocations?
-                            compound = {}
-                            compound['forma'] = a.text
-                            sibling = a.find_next_sibling('p')
-                            compound['order'] = int(sibling.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
-                            compound['category'] = sibling.find('abbr', attrs={'class': ['d', 'g']})['title']
-                            
-                            if sibling.find('abbr', attrs={'class': 'c'}):
-                                compound['country'] = sibling.find('abbr', attrs={'class': 'c'})['title']
-                                definition_content = re.findall('.*?[.]', sibling.text)
-                                definition_text = ""
-                                for i in definition_content:
-                                    if(len(i) > 8):
-                                        definition_text += i
-                                                            
-                                compound['definition'] = definition_text.strip()
-                            word['compound'].append(compound)
+                        definition['definition'] = definition_text.strip()
+                        lemma_dict['definitions'].append(definition)
 
-        return json.dumps(word, ensure_ascii=False)  
+                # Get complex words or collocations
+                complex_words = article.findAll('p', attrs={'class': ['k5', 'k6']})
+                if complex_words:
+                    lemma_dict['complex'] = []
+                    for a in complex_words:
+                        complex = {}
+                        complex['forma'] = a.text
+                        sibling = a.find_next_sibling('p')
+                        complex['order'] = int(sibling.find('span', attrs={'class': 'n_acep'}).text.replace('.', ''))
+                        complex['category'] = sibling.find('abbr', attrs={'class': ['d', 'g']})['title']            
+                        if sibling.find('abbr', attrs={'class': 'c'}):
+                            complex['country'] = sibling.find('abbr', attrs={'class': 'c'})['title']
+                        definition_content = re.findall('.*?[.]', sibling.text)
+                        definition_text = ""
+                        for i in definition_content:
+                            if(len(i) > 8):
+                                definition_text += i
+                        complex['definition'] = definition_text.strip()
+                        lemma_dict['complex'].append(complex)
+            
+            word['articles'].append(lemma_dict)
+
+        return {
+            'result': True,
+            'data': word
+            } 
     except:
-        print(f"Error during the transformation of {word} word")
-        
+        print(f"Error during the transformation of {word['lemma']} word")
         return {
             'result': False
         }
 
-
-def load():
-    pass
-
-
+def load(word):
+    try:
+        myclient = pymongo.MongoClient(cred)
+        mydb = myclient["dictionary"]
+        mycol = mydb['lemmasRae']
+        mycol.insert_one(word)
+    except:
+        print(f"Error during the load of {word['lemma']} word")
 
 def main():
-    # print(get_definition_rae('inteligencia'))
-    response = extraction("caravana")
+    data_ext = extraction("poeta")
 
-    if response['result']:
-        data = response['data']
-        t = transform(data)
-        print(t)
-
+    if data_ext['result']:
+        data_tran = transform(data_ext['data'])
+        if data_tran['result']:
+            load(data_tran['data'])
+        
 if __name__ == "__main__":
     main()
