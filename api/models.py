4d0bdb21ddec2553be107793262d91c7e08997d7
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-05-16 11:03:09 +0200

    "Add pycache files"

---
+++ api/models.py
@@ -0,0 +1,10 @@
+﻿import mongoengine as me
+
+class lemmasrae(me.Document):
+    lemma = me.StringField(required=True)
+    # source = me.StringField(require=True)
+    # articles = me.ListField(me.StringField(), require=True)
+    # articles = me.ListField(me.Document(), require=True)
+
+class Crea(me.Document):
+    lemma = me.StringField(required=True)
---
+++ test/oxford.py
@@ -0,0 +1,20 @@
+﻿import requests
+import json
+
+headers = {
+    "app_id": "45be6888",
+    "app_key": "fc28e3b2831f4e5fd7858ffd7dcac97a" 
+}
+# app_id = "45be6888"
+# app_key = "fc28e3b2831f4e5fd7858ffd7dcac97a"
+
+language = "es"
+word_id = "tortilla"
+url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+
+r = requests.get(url, headers=headers)
+
+print("code {}\n".format(r.status_code))
+print("text \n" + r.text)
+print("json \n" + json.dumps(r.json()))
+
