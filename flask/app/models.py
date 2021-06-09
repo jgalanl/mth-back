019056b9ba0b8ce5c376f1e453c933b119349c27
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-06-10 00:44:44 +0200

    Add docker-compose and folder structure

---
+++ flask/app/models.py
@@ -0,0 +1,12 @@
+﻿from mongoengine import Document
+from mongoengine import DateTimeField, StringField, ReferenceField, ListField
+
+class lemmasrae(Document):
+    lemma = StringField(required=True)
+    # source = me.StringField(require=True)
+    # articles = me.ListField(me.StringField(), require=True)
+    # articles = me.ListField(me.Document(), require=True)
+
+class Crea(Document):
+    lemma = StringField(required=True)
+
---
+++ flask/app/views.py
@@ -0,0 +1,5 @@
+﻿from app import app
+
+@app.route("/")
+def index():
+    return "Hello from flask"
