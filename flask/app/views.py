bc36618be999804dcf9c3a0db2daceff27143b5d
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-06-13 20:04:30 +0200

    POST test

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,13 @@
-﻿from app import app
+﻿from http import HTTPStatus
+from app import app
 
 @app.route("/")
 def index():
     return "Hello from flask"
+
+@app.route("/", methods='POST')
+def post():
+    response = {
+        'data': 'post example'
+    }
+    return response, HTTPStatus.OK
