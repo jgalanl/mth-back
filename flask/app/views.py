a5fb8a9ddf53a4db72a8b81201aaadc363c0c436
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-06-17 00:33:34 +0200

    Addded CORS

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,11 +1,14 @@
 ﻿from http import HTTPStatus
+from flask_cors import cross_origin
 from app import app
 
 @app.route("/", methods=['GET'])
+@cross_origin()
 def index():
     return "Hello from flask"
 
 @app.route("/", methods=['POST'])
+@cross_origin()
 def post():
     response = {
         'data': 'post example'
