efbd62ce695e0b77b50abfcc0561cff3de358816
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-10-06 18:21:30 +0200

    Add examples

---
+++ examples/example.js
@@ -0,0 +1,19 @@
+﻿var http = require('http');
+
+const options = {
+    host: 'http://163.117.129.205',
+    port: '80',
+    path: 'api/lemmas/programar',
+    method: 'GET'
+}
+
+http.get(options, (resp) => {
+    let body = '';
+    resp.on('data', (d) => {
+        body += d;
+    });
+    resp.on('end', () => {
+        let parsed = JSON.stringify(body);
+        console.log(parsed)
+    })
+})
